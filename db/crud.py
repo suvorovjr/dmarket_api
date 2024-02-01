a067300d8f6ff31a2954b7d3da7cab89eb64b7bc
exetch <gassemark@gmail.com>
2024-02-01 13:30:15 +0300

    crud.py docstrings added

--- db/crud.py
+++ db/crud.py
@@ -1,11 +1,10 @@
 from typing import List
 from datetime import datetime
-
 from loguru import logger
 from peewee import DoesNotExist
-
 from db.models import Skin, SkinOffer, db
 from api.schemas import SkinHistory, MarketOffer, SellOffer
+
 
 db.connect()
 Skin.create_table()
@@ -14,14 +13,29 @@ db.close()
 
 
 class SelectSkin:
+    """
+    Класс, предоставляющий статические методы для работы с моделью Skin в базе данных.
+    Включает функционал для создания, поиска и обновления записей скинов.
+    """
     @staticmethod
     def create_all_skins(items: List[SkinHistory]):
+        """
+        Создает записи для всех скинов в базе данных.
+
+        :param items: Список объектов SkinHistory для создания записей.
+        """
         skins = [Skin(**i.dict()) for i in items]
         with db.atomic():
             Skin.bulk_create(skins, batch_size=500)
 
     @staticmethod
     def skin_existence(item: MarketOffer):
+        """
+        Проверяет наличие скина в базе данных по названию.
+
+        :param item: Объект MarketOffer, содержащий название скина для проверки.
+        :return: True, если скин существует в базе данных, иначе False.
+        """
         skin = Skin.select().where(Skin.title == item.title)
         if skin:
             return True
@@ -29,6 +43,11 @@ class SelectSkin:
 
     @staticmethod
     def find_by_name(items: List[SkinHistory]):
+        """
+        Обновляет или создает записи скинов в базе данных на основе предоставленного списка.
+
+        :param items: Список объектов SkinHistory для обновления или создания.
+        """
         skins_to_update = list()
         skin_to_create = list()
         for item in items:
@@ -50,11 +69,23 @@ class SelectSkin:
 
     @staticmethod
     def select_all() -> List[SkinHistory]:
+        """
+        Возвращает список всех скинов из базы данных.
+
+        :return: Список объектов SkinHistory.
+        """
         skins = Skin.select()
         return [SkinHistory.from_orm(skin) for skin in skins]
 
     @staticmethod
     def select_update_time(now, delta) -> List[SkinHistory]:
+        """
+        Возвращает список скинов, которые не обновлялись в течение определенного времени.
+
+        :param now: Текущее время в формате timestamp.
+        :param delta: Временной интервал в секундах, в течение которого скины не обновлялись.
+        :return: Список объектов SkinHistory.
+        """
         skins = Skin.select().where(Skin.update_time < datetime.fromtimestamp(now - delta))
         if skins:
             return [SkinHistory.from_orm(skin) for skin in skins]
@@ -62,9 +93,17 @@ class SelectSkin:
 
 
 class SelectSkinOffer:
-
+    """
+    Класс, предоставляющий статические методы для работы с моделью SkinOffer в базе данных.
+    Включает функционал для создания, обновления и удаления записей предложений скинов.
+    """
     @staticmethod
     def create_skin(item: SellOffer) -> None:
+        """
+        Создает новую запись предложения скина в базе данных.
+
+        :param item: Объект SellOffer, содержащий информацию для создания записи.
+        """
         new_skin = SkinOffer.create(
             title=item.title,
             game=item.game,
@@ -79,6 +118,11 @@ class SelectSkinOffer:
 
     @staticmethod
     def update_sold(skins: List[SkinOffer]):
+        """
+        Обновляет информацию о проданных скинах в базе данных.
+
+        :param skins: Список объектов SkinOffer для обновления.
+        """
         if skins:
             with db.atomic():
                 SkinOffer.bulk_update(skins, fields=[SkinOffer.title, SkinOffer.sellPrice,
@@ -87,22 +131,40 @@ class SelectSkinOffer:
             logger.debug("Список skins пустой. Нет объектов для обновления.")
     @staticmethod
     def select_not_sell() -> List[SellOffer]:
+        """
+        Возвращает список предложений скинов, которые еще не были проданы.
+
+        :return: Список объектов SellOffer.
+        """
         skins = SkinOffer.select().where(SkinOffer.sellTime == None)
         return [SellOffer.from_orm(s) for s in skins]
 
     @staticmethod
     def select_all() -> List[SkinOffer]:
+        """
+        Возвращает список всех предложений скинов из базы данных.
+
+        :return: Список объектов SkinOffer.
+        """
         skins = SkinOffer.select()
         return skins
 
     @staticmethod
     def delete_all():
+        """
+        Удаляет все записи предложений скинов из базы данных.
+        """
         skins = SkinOffer.select()
         for s in skins:
             s.delete_instance()
 
     @staticmethod
     def update_by_asset(skin: SellOffer):
+        """
+        Обновляет запись предложения скина в базе данных, используя AssetID.
+
+        :param skin: Объект SellOffer, содержащий обновленную информацию.
+        """
         try:
             item = SkinOffer.get(SkinOffer.AssetID == skin.AssetID)
             item.OfferID = skin.OfferID
@@ -114,6 +176,11 @@ class SelectSkinOffer:
 
     @staticmethod
     def update_offer_id(skin: SellOffer):
+        """
+        Обновляет идентификатор предложения и другую информацию скина в базе данных.
+
+        :param skin: Объект SellOffer, содержащий обновленную информацию.
+        """
         try:
             item = SkinOffer.get(SkinOffer.AssetID == skin.AssetID)
             item.OfferID = skin.OfferID
